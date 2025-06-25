f74a4a54e674d415db41973d77d26eae4b59019b
CCXLV <abracadabragio12@gmail.com>
2025-06-25 14:04:15 +0400

    Update database path

--- src/config/index.ts
+++ src/config/index.ts
@@ -15,7 +15,7 @@ class Config {
   public static readonly LOTTIE_SYNC_DIR = 'lottie-sync';
   public static readonly LOTTIE_SYNC_DIR_PATH = path.join(this.STATIC_DIR, this.LOTTIE_SYNC_DIR);
 
-  public static readonly DB_PATH = path.join(this.BASE_DIR, process.env.DB_FILE_NAME || 'local.db');
+  public static readonly DB_PATH = path.join(this.BASE_DIR, process.env.DB_FILE_NAME || 'database.db');
 }
 
 export default Config;

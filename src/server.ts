cb76ebb8ae33a51fe1fbc0436df1506192704f8d
CCXLV <abracadabragio12@gmail.com>
2025-06-26 03:06:09 +0400

    Update cors

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:10086'],
+    origin: ['http://localhost:5173', 'http://localhost:10086', 'http://localhost:80'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['*'],

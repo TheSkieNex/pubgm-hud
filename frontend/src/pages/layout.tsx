3f5d478b4418bb8c27021f0a19cfb85d07a29ff7
ccxlv <abracadabragio12@gmail.com>
2025-06-28 00:43:49 +0400

    Add dropdown menu for easy copies

--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import { Toaster } from '@/components/ui/sonner';
 
 export default function Layout() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Outlet />
+      <Toaster />
     </div>
   );
 }
--- frontend/src/pages/tables/page.tsx
+++ frontend/src/pages/tables/page.tsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from 'react';
+import { EllipsisVertical, Clipboard } from 'lucide-react';
+import { toast } from 'sonner';
 
 import type { Table } from '@/lib/types';
 import { API_BASE_URL } from '@/lib/api';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 export default function TablesPage() {
   const [tables, setTables] = useState<Table[]>([]);
@@ -15,6 +24,11 @@ export default function TablesPage() {
     fetchTables();
   }, []);
 
+  const handleCopy = (text: string) => {
+    navigator.clipboard.writeText(text);
+    toast.success('Copied to clipboard');
+  };
+
   return (
     <div className="w-full h-full flex flex-col items-center justify-center gap-4 p-4">
       <h1 className="text-2xl font-bold">Tables</h1>
@@ -27,7 +41,25 @@ export default function TablesPage() {
             <a href={`/table/${table.uuid}`} className="flex items-start flex-1 p-2">
               {table.name}
             </a>
-            {/* TODO: Add dropdown menu, after adding auth */}
+            <DropdownMenu>
+              <DropdownMenuTrigger className="cursor-pointer p-2">
+                <EllipsisVertical size={16} />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem
+                  onClick={() => handleCopy(`${window.location.origin}/elimination/${table.uuid}`)}
+                >
+                  <Clipboard size={16} />
+                  Copy Elimination URL
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onClick={() => handleCopy(`${window.location.origin}/table/${table.uuid}`)}
+                >
+                  <Clipboard size={16} />
+                  Copy Table URL
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         ))}
       </div>

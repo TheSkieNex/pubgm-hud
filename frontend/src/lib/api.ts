d3d4c8283f1fa81b87f5ce94834ad15f1ca6c374
ccxlv <abracadabragio12@gmail.com>
2025-06-29 18:06:11 +0400

    Add env files in frontend and update BASE_URL in api

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,8 +1,10 @@
 import { io } from 'socket.io-client';
 
-export const API_BASE_URL = 'http://localhost:3011';
+const ENV = import.meta.env;
 
-export const socket = io('ws://localhost:3011', {
+export const API_BASE_URL = ENV.VITE_API_BASE_URL || 'http://localhost:3011';
+
+export const socket = io(API_BASE_URL, {
   withCredentials: true,
   transports: ['websocket'],
 });

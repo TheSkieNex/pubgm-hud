6f661876731284183fb28fb8042abd619c5b4174
ccxlv <abracadabragio12@gmail.com>
2025-06-30 22:29:13 +0400

    Fix API url in frontend

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 const ENV = import.meta.env;
 
 export const API_BASE_URL = ENV.VITE_API_BASE_URL || 'http://localhost:3011';
-export const API_URL = `${API_BASE_URL}`;
+export const API_URL = `${API_BASE_URL}/router`;
 
 export const socket = io(API_BASE_URL, {
   path: '/api/socket.io',

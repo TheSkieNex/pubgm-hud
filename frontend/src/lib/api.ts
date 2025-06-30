336ed330bdeb7d65503bc9fb64da22886c717040
ccxlv <abracadabragio12@gmail.com>
2025-06-30 22:19:33 +0400

    Updates

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,9 +3,10 @@ import { io } from 'socket.io-client';
 const ENV = import.meta.env;
 
 export const API_BASE_URL = ENV.VITE_API_BASE_URL || 'http://localhost:3011';
-export const API_URL = `${API_BASE_URL}/router`;
+export const API_URL = `${API_BASE_URL}`;
 
 export const socket = io(API_BASE_URL, {
+  path: '/api/socket.io',
   withCredentials: true,
   transports: ['websocket'],
 });

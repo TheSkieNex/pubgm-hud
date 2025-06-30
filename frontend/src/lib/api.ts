102040751cef4834821b09a0fec2572141419d51
ccxlv <abracadabragio12@gmail.com>
2025-06-30 22:36:54 +0400

    Update socketio nginx configuration

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -6,7 +6,7 @@ export const API_BASE_URL = ENV.VITE_API_BASE_URL || 'http://localhost:3011';
 export const API_URL = `${API_BASE_URL}/router`;
 
 export const socket = io(API_BASE_URL, {
-  path: '/api/socket.io',
+  path: '/socket.io',
   withCredentials: true,
   transports: ['websocket'],
 });

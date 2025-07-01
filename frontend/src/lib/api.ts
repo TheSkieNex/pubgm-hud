b05bfd15dd81d5eca352e12f94b3a682d343dab1
ccxlv <abracadabragio12@gmail.com>
2025-07-01 13:55:42 +0400

    Update frontend envrionments

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,8 +2,10 @@ import { io } from 'socket.io-client';
 
 const ENV = import.meta.env;
 
+const isProduction = ENV.VITE_ENV === 'production';
+
 export const API_BASE_URL = ENV.VITE_API_BASE_URL || 'http://localhost:3011';
-export const API_URL = `${API_BASE_URL}/api`;
+export const API_URL = isProduction ? `${API_BASE_URL}/api` : API_BASE_URL;
 export const ROUTER_URL = `${API_BASE_URL}/router`;
 
 export const socket = io(API_BASE_URL, {

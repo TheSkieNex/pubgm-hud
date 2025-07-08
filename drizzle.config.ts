c0bb125376bd975b45f4a26e6ccf34b9452d3390
CCXLV <abracadabragio12@gmail.com>
2025-07-08 22:22:46 +0400

    Add todo to fix drizzle schemas path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,8 @@ import path from 'node:path';
 
 import { defineConfig } from 'drizzle-kit';
 
+// Schema path doesn't work on windows, but ./src/db/schemas works 
+// TODO: FIX THIS
 const SCHEMA_PATH = path.join(__dirname, 'src', 'db', 'schemas');
 const DB_FILE_PATH =
   process.env.NODE_ENV === 'production'

1bd99c9939cc7a236feab226667eb7881965f98c
George Merebashvili <abracadabragio12@gmail.com>
2025-08-01 19:02:53 +0400

    Remove custom endpoints for general use

--- src/db/schemas/custom.ts
+++ src/db/schemas/custom.ts
@@ -1,11 +1,1 @@
-import { integer, sqliteTable } from 'drizzle-orm/sqlite-core';
-
-export const overallResultsPoints = sqliteTable('overall_results_points', {
-  id: integer().primaryKey(),
-  tableId: integer('table_id').notNull(),
-  teamId: integer('team_id').notNull(),
-  wwcd: integer('wwcds').notNull().default(0),
-  placementPts: integer('placement_pts').notNull().default(0),
-  elims: integer('elims').notNull().default(0),
-  total: integer('total').notNull().default(0),
-});
+// Add your custom schemas here
--- src/routers/custom.ts
+++ src/routers/custom.ts
@@ -4,18 +4,8 @@ import CustomController from '../controllers/custom';
 
 const customRouter = Router();
 
-customRouter.put('/wwcd-team', CustomController.updateWWCDTeam);
-customRouter.put('/match-results', CustomController.updateMatchResult);
-customRouter.put('/overall-results', CustomController.updateOverallResults);
-customRouter.put('/overall-results-absolute', CustomController.updateOverallResultsAbsolute);
-customRouter.delete(
-  '/:tableUUID/reset-match-results/:fileUUID',
-  CustomController.resetMatchResults
-);
-customRouter.delete(
-  '/:tableUUID/reset-overall-results/:fileUUID',
-  CustomController.resetOverallResults
-);
-customRouter.put('/update-map-rotation', CustomController.updateMapRotation);
+// Add your custom endpoints here
+// Example:
+// customRouter.put('/update-map-view', CustomController.updateMapView);
 
 export default customRouter;
